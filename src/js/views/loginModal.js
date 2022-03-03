9c15e81f74a657645fa866d855ecc4ae7f479ba4
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-03-03 17:19:35 +0100

    Modal styles changes

--- src/js/views/loginModal.js
+++ src/js/views/loginModal.js
@@ -2,7 +2,7 @@ import * as constants from '../constants.js';
 
 // When the user clicks on the button, open the modal
 export const openModal = function () {
-  constants.modal.style.display = 'block';
+  constants.modal.style.display = 'flex';
 };
 
 // When the user clicks on <span> (x), close the modal
